62f6c2171e8d05769a9ce72588dfd07553e9eabe
Kabeer Khan <kdotkhan22@gmail.com>
2025-11-11 21:52:33 +0500

    Dynamic URLs

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('helloworld/', views.helloWorld),
+    path('home/', views.home),
+    path("<int:id>/", views.post),
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -2,5 +2,35 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def helloWorld(request):
-    return HttpResponse("Hello World")
+posts = [
+    {
+        'id': 1,
+        'title': 'Explore Python',
+        'content': 'Python is a versatile programming language that can be used for web development, data science, and artificial intelligence.',
+    },
+    {
+        'id': 2,
+        'title': 'Explore Django',
+        'content': 'Django is a web framework for building web applications using Python.',
+    },
+    {
+        'id': 3,
+        'title': 'Explore JavaScript',
+        'content': 'JavaScript is a programming language that can be used for web development.',
+    },
+]
+
+
+def home(request):
+    html = ""
+    for post in posts:
+        html += f"""
+        <div>
+            <h1>{post['id']} - {post['title']}</h1>
+            <p>{post['content']}</p>
+        </div>
+        """
+    return HttpResponse(html)
+
+def post(request, id):
+    return HttpResponse(f"Post {id}")
